10e9d7ce47a3aeca3c750941be241e955ece9eb3
happy <happy@DESKTOP-HQUE00G>
2019-10-07 18:17:52 +0900

    10/07

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,22 +1,15 @@
+/*백준 19051*/
 package java;
-/*백준 1330문제*/
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int a = 0;
-		int b = 0;
-		Scanner scan = new Scanner(System.in);
-		
-		a= scan.nextInt();
-		b= scan.nextInt();
-		
-		if(a>b) {System.out.println(">");}
-		else if(a==b) {System.out.println("==");}
-		else {System.out.println("<");}
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A + B);
+		}
 	}
-
 }
