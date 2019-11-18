a9198add7c78e56eeecb5fc353b5a5cbc7e4feec
happy <happy@DESKTOP-HQUE00G>
2019-11-18 15:50:02 +0900

    11/18

---
+++ src/java/Alpha.java
@@ -0,0 +1,13 @@
+//백준 알고리즘 10809
+package java;
+
+import java.util.Scanner;
+public class Alpha {
+       public static void main(String[] args) {
+             Scanner sc = new Scanner(System.in);
+             String input = sc.next();
+             
+             for (char c = 'a' ; c <= 'z' ; c++)
+                    System.out.print(input.indexOf(c) + " ");
+       }
+}
